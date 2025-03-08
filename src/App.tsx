531c81785bf7f269ac5466433c80d10356685bd3
Rami Aljanaby <ramialj94@yahoo.com>
2025-03-08 10:44:12 -0600

    fixed button overlapping in mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,16 @@
-import { useState } from 'react'
 
-import './App.css'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+
 
 //I added this card component from https://ui.shadcn.com/docs/components/
 //Just an example, we could have multiple cards holding articles 
 
-/*function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-<div>
-  <Card>
-    <CardHeader>
-      <CardTitle>Article Title</CardTitle>
-      <CardDescription>Article Description</CardDescription>
-    </CardHeader>
-    <CardContent>
-      <p>Article Content</p>
-    </CardContent>
-    <CardFooter>
-      <p>Article Footer</p>
-    </CardFooter>
-  </Card>
-</div>       
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}*/
 
 //Using the NewsList in the App
 import React from 'react';
 import NewsList from './components/ui/NewsList';
 import CategoryFilter from './components/ui/CategoryFilter';
 import { useParams } from "react-router";
+import './App.css';
 import { Redirect } from 'react-router-dom'
 
 const App: React.FC = () => {
--- src/components/ui/CategoryFilter.tsx
+++ src/components/ui/CategoryFilter.tsx
@@ -1,13 +1,24 @@
-const CategoryFilter = ({ currentCategory, setCategory }: { currentCategory: string, setCategory: (category: string) => void }) => {
+const CategoryFilter = ({
+  currentCategory,
+  setCategory,
+}: {
+  currentCategory: string;
+  setCategory: (category: string) => void;
+}) => {
   const categories = ["general", "sports", "technology", "business"];
 
   return (
-    <div className="flex space-x-2 mb-4">
+    <div className="flex flex-wrap gap-2 justify-center md:justify-start mb-4">
       {categories.map((category) => (
         <a
           key={category}
           href={"/" + category}
-          className={"px-4 py-2 " + (currentCategory === category ? "bg-sky-500 text-white hover:bg-sky-600 hover:text-white" : "bg-gray-200 text-black hover:bg-gray-300 hover:text-black") + " rounded capitalize"}
+          className={
+            "px-4 py-2 rounded capitalize " +
+            (currentCategory === category
+              ? "bg-sky-500 text-white hover:bg-sky-600"
+              : "bg-gray-200 text-black hover:bg-gray-300")
+          }
         >
           {category}
         </a>
